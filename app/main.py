1da9a13e1028988ca750ef81a2e835634799ec51
Nay Lin Aung <naylinaung.nla.dev@gmail.com>
2025-05-09 00:09:28 +0700

    Update .env config

--- app/main.py
+++ app/main.py
@@ -1,12 +1,34 @@
 from fastapi import FastAPI
 import uvicorn
+from dotenv import load_dotenv
+import os
+import socket
+
+# Load environment variables from .env
+load_dotenv()
 
 app = FastAPI()
+
+# Read environment variables
+APP_NAME = os.getenv("APP_NAME", "Unknown App")
+APP_VERSION = os.getenv("APP_VERSION", "0.0.0")
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello from FastAPI CICD Project!"}
+
+
+@app.get("/health")
+async def health():
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    return {
+        "app_name": APP_NAME,
+        "app_version": APP_VERSION,
+        "container_hostname": hostname,
+        "container_ip": ip_address
+    }
 
 
 if __name__ == "__main__":
